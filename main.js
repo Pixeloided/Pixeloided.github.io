d267832a566cee9ae54d7f7c8860e7378debf1e6
Pixeloided <pixeloidsemail@gmail.com>
2021-06-17 15:30:05 -0700

    Stuff

---
+++ main.js
@@ -0,0 +1,7 @@
+const html = document.documentElement;
+const canvas = document.querySelector('.video-scrolling');
+const context = canvas.getContext('2d');
+
+const currentFrame = index => (
+    `https://www.`
+)
