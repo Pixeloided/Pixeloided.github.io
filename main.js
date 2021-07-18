7a597bed7a4d474cb5bbc2a7b078673076b54312
Pixeloided <pixeloidsemail@gmail.com>
2021-07-17 21:34:02 -0700

    Redid website

--- main.js
+++
@@ -1,7 +0,0 @@
-const html = document.documentElement;
-const canvas = document.querySelector('.video-scrolling');
-const context = canvas.getContext('2d');
-
-const currentFrame = index => (
-    `https://www.`
-)
