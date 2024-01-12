1ba1996c763626b54b428da4301f52fd9ae2b050
Pixeloided <r.dev.email.test@gmail.com>
2024-01-11 19:25:58 -0800

    Add navbar, Fix footer issues.

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ document.addEventListener("scroll", function () {
   const parallax = document.querySelectorAll(".parallax");
   scrollY = window.scrollY + 1;
 
+  if (scrollY > 120) {
+    const arrow = document.querySelector(".scroll-arrow");
+    arrow.style.opacity = 0;
+    arrow.style.scale = 0.75;
+  }
+
   parallax.forEach((item, index) => {
     item.style.transform = `translateY(${scrollY * item.dataset.speed}px)`;
   });
