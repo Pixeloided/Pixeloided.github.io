112d4c2311b1b459759e2213e0cace9b3447c9a3
Pixeloided <r.dev.email.test@gmail.com>
2024-01-11 22:59:52 -0800

    Delete unused images and add animation to elements

--- index.js
+++ index.js
@@ -23,3 +23,14 @@ document.addEventListener("scroll", function () {
     item.style.transform = `translateY(${scrollY * item.dataset.speed}px)`;
   });
 });
+
+document.addEventListener("mousemove", function (e) {
+  const moveElement = document.querySelectorAll(".mmove");
+  moveElement.forEach((elem, index) => {
+    const moveRateX = elem.getAttribute("data-rate-x") || 0.05;
+    const moveRateY = elem.getAttribute("data-rate-y") || 0.05;
+    const x = (window.innerWidth / 2 - e.pageX) * moveRateX;
+    const y = (window.innerHeight / 2 - e.pageY) * moveRateY;
+    elem.style.transform = `translate(${x}px, ${y}px)`;
+  });
+});
